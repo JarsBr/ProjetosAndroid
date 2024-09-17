34cc7b989ec29d1f34af5dc68103756996ca0bdb
José Antonio Ribeiro Souto <56361749+JarsBr@users.noreply.github.com>
2024-09-17 10:20:24 -0300

    17/09

---
+++ MyApplication2/app/src/main/java/com/example/myapplication2/CreateMessageActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CreateMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val editText = findViewById<EditText>(R.id.editText_name)
+        val buttonSend = findViewById<Button>(R.id.button_enviar)
+
+        buttonSend.setOnClickListener {
+            val message = editText.text.toString()
+            val intent = Intent(this, ReceiveMessageActivity::class.java).apply {
+                putExtra("mensagem", message)
+            }
+            startActivity(intent)
+    }
+}}
---
+++ MyApplication2/app/src/main/java/com/example/myapplication2/ReceiveMessageActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication2
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ReceiveMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_receive_message)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val intent = intent
+        val mensagemTexto = intent.getStringExtra("mensagem")
+        val textView = findViewById<TextView>(R.id.text_pagina2)
+        textView.text = mensagemTexto
+
+    }
+}
